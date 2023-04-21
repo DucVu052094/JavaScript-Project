175b6dfc5099dc9673c00efb6df7e3d2e9061e8c
DucVu052094 <vuhongduc205@gmail.com>
2023-04-21 02:15:27 -0700

    added main.js and index.html

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+var A= "This is long string"; //assign a string variable
+
+window.alert(A); //using window alert to do a popup box of variable A
+
+var B = "This is the beginning of the string" , C =" and this is the end of the string" //Assigning multiple variables in 1 line
+
+var D = B+C; //concatenating 2 string variables
+
+document.write(10*2); //writing an expression
+
+document.write("<br>"); //adds a line break
+
+document.write(D);  //writing out variable D

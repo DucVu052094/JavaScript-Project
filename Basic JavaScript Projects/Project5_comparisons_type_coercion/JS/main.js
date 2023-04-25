42b0b4697353271c4280e8c16aeb88980627090e
DucVu052094 <vuhongduc205@gmail.com>
2023-04-25 01:45:34 -0700

    Added operators code

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,17 @@
+document.write(typeof "Bear"); //Using typeof operator
+
+document.write("five is this number:" + 5); //combining string and number type
+
+document.write(10 == 11); //display false using == operator
+
+document.write("10"===10); //display false using === operator
+
+document.write(5<7); //display true using less than sign
+
+document.write(5>3); //display true using greater than sign
+
+document.write(5!=4); //display true using not operator
+
+document.write(10>2 && 5<7) //display true using and operator
+
+document.write(6>2 || 3<1) //display true using or operator

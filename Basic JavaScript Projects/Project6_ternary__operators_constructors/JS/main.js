99dc628248c23376fb1ee380dbefc0ad30e91dc7
DucVu052094 <vuhongduc205@gmail.com>
2023-04-25 19:05:02 -0700

    Added comment

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,4 +1,4 @@
-function Ride_Function()
+function Ride_Function()    //Creating a ternary function that use ternary operation
 {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
@@ -6,37 +6,37 @@ function Ride_Function()
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
-function Vote_Function()
+function Vote_Function() //Creating a ternary function that use ternary operation
 {
     var Age, Can_vote;
     Age = document.getElementById("Age").value;
-    Can_vote = (Age<18) ? "You are too young":"You are old enough";
+    Can_vote = (Age<18) ? "You are too young":"You are old enough"; //Check if age is over or under 18
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
 
-function Vehicle(Make, Model, Year, Color){
+function Vehicle(Make, Model, Year, Color){ //Creating a constructor function for Vehicle Class
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
     this.Vehicle_Color = Color;
 }
-
+//Creating new Vehicle objects
 var Jack = new Vehicle("Dodge","Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep","Trail Hawk", 2019,"White and Black");
 var Eric = new Vehicle("Ford", "Pinto", 1971,"Mustard");
 
-function myFunction(){
+function myFunction(){ //function that grabs a vehicle object variables and display them
     document.getElementById("Keywords_and_Constructors").innerHTML =
     "Eric drives a " + Eric.Vehicle_Color + "-colored " + Eric.Vehicle_Model +
     " manufactured in " + Eric.Vehicle_Year;
 }
 
-function myFunction2(){
+function myFunction2(){ //function that grabs a vehicle object variables and display them
     document.getElementById("New_and_This").innerHTML = "Jack has a " +
     Jack.Vehicle_Color + " " + Jack.Vehicle_Make + " made in " + Jack.Vehicle_Year;
 }
 
-function Nested_Function(){
+function Nested_Function(){ //creating a function that has a nested function inside it
     document.getElementById("Nested_Function").innerHTML = DisplayFunction();
     function DisplayFunction(){
         document.write("This is a nested function");
